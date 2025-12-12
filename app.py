772c23e6544960f121a1c9c321986d2e30ed90b3
seulgikim <sgim49697@gmail.com>
2025-12-12 09:12:43 +0900

    flask-mail port replace

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if DATABASE_URL.startswith("postgres://"):
 
 # Flask-Mail 설정
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
